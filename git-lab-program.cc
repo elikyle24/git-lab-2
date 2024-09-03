e0654cd617a6489ce3dbf4e1a668797c42f2f523
Elijah Kyle <ek687722@ohio.edu>
2024-09-03 17:45:12 -0400

    Almost Done

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Elijah Kyle>
+ *        Date: <September 3rd, 2024>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
